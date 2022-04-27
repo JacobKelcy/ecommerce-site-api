b447afa3d8fc640e77d08cd586383c820b54dd72
Jacob Kelcy <jacobkelcy23@gmail.com>
2022-04-27 13:30:41 -0500

    Home page finished

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="hero">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <img
+                src="https://media.istockphoto.com/photos/cropped-portrait-of-a-successful-businessman-dressed-in-an-elegant-picture-id1021850512?k=20&m=1021850512&s=612x612&w=0&h=lH3MlvkGnOJmnB3r_dOBctOBODRsCoiH88E6_KMcFbs="
+                alt="Hero"
+              ></img>
+              <div className="content">
+                <h5 className="title">New Season Arrivals</h5>
+                <p className="subtitle">Check out all the trends</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
